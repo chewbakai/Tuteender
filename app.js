598099c0b08e69f7e1eaf9b51a05082870207d07
kaiceniza <kaiceniza@users.noreply.github.com>
2021-05-22 23:18:58 +0800

    added tuteender assets

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express"),
+      app = express(),
+      session = require("express-session"),
+      http = require("http"),
+      server = http.createServer(app),
+      bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));      
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// const   accountRoutes = require('./routes/accountRoutes'),
+//         taskRoutes = require('./routes/taskRoutes');
 
-var app = express();
+app.set('view engine','ejs')
+app.use(express.static(__dirname + '/public'));
+app.use(session({
+    secret: "12345678",
+    resave: false,
+    saveUninitialized: false    
+}));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.get("/", (req,res) => {
+    res.render("index");
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get("/logout", (req,res) => {
+    req.session.destroy();
+    res.redirect('/');
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// //routes
+// app.use("/account", accountRoutes);
+// app.use("/task", taskRoutes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.listen(8080);
+console.log('Server is listening on port 8080');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
